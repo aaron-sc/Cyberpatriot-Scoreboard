72e938e9bcd17e124a904895434f87666082b475
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-12-14 03:15:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+from flask import Flask, render_template,request,jsonify
+from flask_cors import CORS
+from bs4 import BeautifulSoup
+
+# Imput team nums to monitor
+teamnums = ["13-0562", "13-1243", "13-3301"]
+
+app = Flask(__name__)
+CORS(app)
+
+def get_team_scores(teamnums):
+	data = {}
+	for teamnum in teamnums:
+		url="http://54.243.195.23/team.php?team="+teamnum
+		response = requests.get(url)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		div = soup.findAll("table", {"class": "CSSTableGenerator"})[0]
+		rows = div.findAll('tr')[1]
+		listofdata = []
+		for i, row in enumerate(rows):
+			listofdata.append(row.text)
+		data[listofdata[0]] = listofdata[1:]
+	return data
+
+@app.route('/',methods = ['GET'])
+def home():
+	result = get_team_scores(teamnums)
+	return render_template('index.html', result=result)
+
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=8080, threaded=True)
